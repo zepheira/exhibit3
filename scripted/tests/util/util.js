f5aca3b8dfc0b1de9892e93dc5342cdf002e4a74
Mark Baker <mark@zepheira.com>
2011-06-08 14:04:27 -0400

    Add string extension tests

--- scripted/tests/util/util.js
+++ scripted/tests/util/util.js
@@ -11,3 +11,30 @@ test("round", function() {
     strictEqual(Exhibit.Util.round(66000, 1024), '65536', "Exhibit.Util.round(66000, 1024) === '65536'");
     strictEqual(Exhibit.Util.round(0, 0.1), '0.0', "Exhibit.Util.round(0, 0.1) === '0.0'");
 });
+
+test("Native string extensions", function() {
+    equal("  Hello world".trim(),"Hello world","trim left two");
+    equal("  Hello world ".trim(),"Hello world","trim both uneven");
+    equal(" Hello world ".trim(),"Hello world","trim both even");
+    equal("Hello world  ".trim(),"Hello world","trim right two");
+    equal("Hello world".trim(),"Hello world","trim identity");
+    equal("Hello    world".trim(),"Hello    world","trim identity centre gap");
+
+    ok("Hello world".startsWith("H"),"startsWith char");
+    ok("Hello world".startsWith("He"),"startsWith string");
+    ok("Hello world".startsWith("Hello wor"),"startsWith long string");
+    ok("Hello world".startsWith("Hello world"),"startsWith full string");
+    ok(!" Hello world".startsWith("He"),"startsWith not string");
+    ok(!"Hello world".startsWith("Hello world foobar"),"startsWith not overflow");
+
+    ok("Hello world".endsWith("d"),"endsWith char");
+    ok("Hello world".endsWith("ld"),"endsWith string");
+    ok("Hello world".endsWith("llo world"),"endsWith long string");
+    ok("Hello world".endsWith("Hello world"),"endsWith full string");
+    ok(!"Hello world ".endsWith("ld"),"endsWith not string");
+    ok(!"Hello world".endsWith("Hello world foobar"),"endsWith not overflow");
+
+    var text = 'The %0 and the %1, the %3 jumped over the %2 \\%3%5.';
+    var subs = ['cat', 'fiddle', 'moon', 'cow'];
+    equal(String.substitute(text, subs),"The cat and the fiddle, the cow jumped over the moon %3%5.","substitute");
+});
