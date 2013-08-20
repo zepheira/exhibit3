1d153ad56667e84549a896b9d40d05c1b08be9f9
Ryan Lee <ryanlee@zepheira.com>
2013-08-19 19:54:47 -0700

    adding openlayers-based version of demo

---
+++ scripted/demos/presidents/main-openlayers.js
@@ -0,0 +1,21 @@
+define('gmaps', ['async!https://maps.googleapis.com/maps/api/js?v=3&sensor=false'],
+    function() {
+        return window.google;
+    }
+);
+
+require(["../api/configs/dev-config.js"], function() {
+    requirejs.config({
+        "paths": {
+            "ext/openlayers/openlayers-extension": "extensions/openlayers/openlayers-extension"
+        }
+    });
+    require(["lib/jquery", "exhibit", "ext/openlayers/openlayers-extension", "ext/time/time-extension"], function($, Exhibit, MapExtension, TimeExtension) {
+        window.Exhibit = Exhibit;
+        Exhibit.MapExtension = MapExtension;
+        Exhibit.OLMapView = MapExtension.OLMapView;
+        Exhibit.TimeExtension = TimeExtension;
+        Exhibit.TimelineView = TimeExtension.TimelineView;
+        $(document).trigger("scriptsLoaded.exhibit");
+    });
+});
