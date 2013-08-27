24e9b04fb0dcf214f136c02b53a4a218862b54d8
Ryan Lee <ryanlee@zepheira.com>
2013-08-26 21:47:59 -0700

    adding pie chart demo with UN MDG data

---
+++ scripted/demos/mdg4/main.js
@@ -0,0 +1,16 @@
+require(["../api/configs/dev-config.js"], function() {
+    requirejs.config({
+        "paths": {
+            "ext/openlayers/openlayers-extension": "extensions/openlayers/openlayers-extension",
+            "ext/flot/flot-extension": "extensions/flot/flot-extension"
+        }
+    });
+    require(["lib/jquery", "exhibit", "ext/openlayers/openlayers-extension", "ext/flot/flot-extension"], function($, Exhibit, MapExtension, FlotExtension) {
+        window.Exhibit = Exhibit;
+        Exhibit.MapExtension = MapExtension;
+        Exhibit.OLMapView = MapExtension.OLMapView;
+        Exhibit.FlotExtension = FlotExtension;
+        Exhibit.PieChartView = FlotExtension.PieChartView;
+        $(document).trigger("scriptsLoaded.exhibit");
+    });
+});
