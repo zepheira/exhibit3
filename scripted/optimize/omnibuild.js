146daecf87ee54e1e2cf36c90fa66b5a4c00e7ac
Ryan Lee <ryanlee@zepheira.com>
2013-09-16 13:36:47 -0700

    build a non-published, require-dependent bundle of everything

---
+++ scripted/optimize/omnibuild.js
@@ -0,0 +1,59 @@
+({
+    "baseUrl": "../src/",
+    "name": "exhibit",
+    "include": [
+        "ext/map/map-extension",
+        "ext/time/time-extension",
+        "ext/openlayers/openlayers-extension",
+        "ext/flot/flot-extension"
+    ],
+    "paths": {
+        "ext": "extensions",
+        "lib": "lib",
+        "nls": "nls",
+        "i18n": "lib/i18n",
+        "async": "lib/async",
+        "openlayers": "extensions/openlayers/lib/OpenLayers",
+        "timeline": "empty:",
+        "gmaps": "empty:"
+    },
+    "shim": {
+        "lib/jquery": {
+            "exports": "jQuery"
+        },
+        "lib/sprintf": {
+            "exports": "vsprintf"
+        },
+        "lib/base64": {
+            "exports": "Base64"
+        },
+        "lib/jquery.history": {
+            "deps": ["lib/jquery"],
+            "exports": "History"
+        },
+        "lib/jquery.history.shim": {
+            "deps": ["lib/jquery.history"]
+        },
+        "lib/jquery.nouislider": {
+            "deps": ["lib/jquery"]
+        },
+        "openlayers": {
+            "exports": "OpenLayers"
+        },
+        "extensions/flot/lib/jquery.flot": {
+            "deps": ["lib/jquery"]
+        },
+        "extensions/flot/lib/jquery.flot.pie": {
+            "deps": ["extensions/flot/lib/jquery.flot"]
+        },
+        "extensions/flot/lib/jquery.flot.axislabels": {
+            "deps": ["extensions/flot/lib/jquery.flot"]
+        },
+        "extensions/flot/lib/jquery.flot.navigate": {
+            "deps": ["extensions/flot/lib/jquery.flot"]
+        },
+        "extensions/flot/lib/jquery.flot.resize": {
+            "deps": ["extensions/flot/lib/jquery.flot"]
+        }
+    }
+})
